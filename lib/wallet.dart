e4521269d6758e6758ceae8a2b23ccf83eb3a170
Mahdi K. Fard <mkfard@outlook.com>
2021-08-14 22:48:55 +0430

    Initial commit.

---
+++ lib/wallet.dart
@@ -0,0 +1,13 @@
+library wallet;
+
+/// Represents a private key.
+class PrivateKey {
+  /// Creates a random private key.
+  PrivateKey.random();
+}
+
+/// Represents a public key.
+class PublicKey {
+  /// Private contructor.
+  PublicKey._();
+}
---
+++ test/wallet_test.dart
@@ -0,0 +1,12 @@
+import 'package:test/test.dart';
+
+//import 'package:wallet/wallet.dart';
+
+void main() {
+  test('adds one to input values', () {
+    // final calculator = PrivateKey.random();
+    // expect(calculator.addOne(2), 3);
+    // expect(calculator.addOne(-7), -6);
+    // expect(calculator.addOne(0), 1);
+  });
+}
