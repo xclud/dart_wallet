2ed7f6cb71a95475a84af519f00e9b0e00c93ced
Mahdi K. Fard <mkfard@outlook.com>
2021-12-20 13:51:31 +0330

    secp256k1 and secp256r1 are now more complete.

--- lib/src/secp256k1.dart
+++ lib/src/secp256k1.dart
@@ -1,4 +1,7 @@
+import 'dart:typed_data';
+
 import "package:pointycastle/export.dart" as p;
+import 'package:wallet/src/der.dart';
 import 'package:wallet/src/private_key.dart';
 import 'package:wallet/src/public_key.dart';
 
@@ -12,4 +15,15 @@ PublicKey createPublicKey(PrivateKey privateKey, bool compressed) {
   final publicParams = p.ECPublicKey(q, _domainParams);
 
   return PublicKey(publicParams.Q!.getEncoded(compressed));
+}
+
+Uint8List generateSignature(PrivateKey privateKey, Uint8List message) {
+  var signer = p.ECDSASigner();
+
+  var priv =
+      p.PrivateKeyParameter(p.ECPrivateKey(privateKey.value, _domainParams));
+  signer.init(true, priv);
+  var rs = signer.generateSignature(message);
+
+  return toDER(rs as p.ECSignature);
 }
--- lib/src/secp256r1.dart
+++ lib/src/secp256r1.dart
@@ -1,5 +1,30 @@
-import "package:pointycastle/export.dart";
+import 'dart:typed_data';
 
-final _domainParams = ECCurve_secp256r1();
-final _keyParams = ECKeyGeneratorParameters(_domainParams);
-final _generator = ECKeyGenerator();
+import "package:pointycastle/export.dart" as p;
+import 'package:wallet/src/der.dart';
+import 'package:wallet/src/private_key.dart';
+import 'package:wallet/src/public_key.dart';
+
+final _domainParams = p.ECCurve_secp256r1();
+final _keyParams = p.ECKeyGeneratorParameters(_domainParams);
+final _generator = p.ECKeyGenerator()..init(_keyParams);
+
+PublicKey createPublicKey(PrivateKey privateKey, bool compressed) {
+  final q = _domainParams.G * privateKey.value;
+
+  final publicParams = p.ECPublicKey(q, _domainParams);
+
+  return PublicKey(publicParams.Q!.getEncoded(compressed));
+}
+
+
+Uint8List generateSignature(PrivateKey privateKey, Uint8List message) {
+  var signer = p.ECDSASigner();
+
+  var priv =
+      p.PrivateKeyParameter(p.ECPrivateKey(privateKey.value, _domainParams));
+  signer.init(true, priv);
+  var rs = signer.generateSignature(message);
+
+  return toDER(rs as p.ECSignature);
+}
